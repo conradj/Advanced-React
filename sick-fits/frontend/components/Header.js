a053ed459772c58aeec90ab54fe5a7cc13f78916
conradj <conradsjackson@gmail.com>
2019-07-09 12:16:38 +0100

    router progress bar

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,19 @@
 import Link from "next/link";
 import Nav from "./Nav";
 import styled from "styled-components";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  console.log("onRouteChangeError triggered");
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -46,7 +59,7 @@ const Header = () => {
       <div className="bar">
         <Logo>
           <Link href="/">
-            <a>Sick Fits</a>
+            <a>SICK FITS</a>
           </Link>
         </Logo>
         <Nav />
