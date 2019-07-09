77a183e961b6b6c86abae52d626643890e466e0d
conradj <conradsjackson@gmail.com>
2019-07-09 15:15:51 +0100

    do some gql yoga

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,20 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // check they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  }
+  //   createDog(parent, args, ctx, info) {
+  //     console.log(args);
+  //   }
+};
 
 module.exports = mutations;
