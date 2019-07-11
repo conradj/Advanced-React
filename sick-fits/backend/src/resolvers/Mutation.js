fd64bca7900b73b64e92ac44f775a462a09befa3
conradj <conradsjackson@gmail.com>
2019-07-11 14:35:42 +0100

    backend password reset flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { randomBytes } = require("crypto");
+const { promisify } = require("util");
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // check they are logged in
@@ -74,6 +76,50 @@ const Mutations = {
   async signout(parent, args, ctx, info) {
     ctx.response.clearCookie("token");
     return { message: "goodbye" };
+  },
+  async requestReset(parent, { email }, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No user registered with email ${email}`);
+    }
+    const randomBytesPromisified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisified(20)).toString("hex");
+    const resetTokenExpiry = Date.now() + 3600000;
+    const res = await ctx.db.mutation.updateUser({
+      where: { email },
+      data: { resetToken, resetTokenExpiry }
+    });
+    console.log(res);
+    return { message: "thanks" };
+  },
+  async resetPassword(
+    parent,
+    { resetToken, password, confirmPassword },
+    ctx,
+    info
+  ) {
+    if (password !== confirmPassword) {
+      throw new Error(`Passwords do not match`);
+    }
+
+    const [user] = await ctx.db.query.users({
+      where: { resetToken, resetTokenExpiry_gte: Date.now() - 3600000 }
+    });
+
+    if (!user) {
+      throw new Error(`Invalid password reset link. Please request a new one`);
+    }
+    const newPassword = await bcrypt.hash(password, 10);
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { id: user.id },
+      data: { password: newPassword, resetToken: null, resetTokenExpiry: null }
+    });
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year
+    });
+    return updatedUser;
   }
 };
 
