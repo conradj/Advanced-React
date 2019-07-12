23ee4e23a64836e9cf0b595f82b2102ac81acfe0
conradj <conradsjackson@gmail.com>
2019-07-12 14:59:01 +0100

    show and hide cart using apollo clientState

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,23 +1,46 @@
 import React from "react";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
 import CartStyle from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
 import CloseButton from "./styles/CloseButton";
 import SickButton from "./styles/SickButton";
 
-const Cart = () => {
-  return (
-    <CartStyle open>
-      <header>
-        <CloseButton title="close">&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You have ___ items in your cart.</p>
-      </header>
-      <footer>
-        <p>$10.10</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </CartStyle>
-  );
-};
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
+
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
+
+const Cart = () => (
+  <Mutation mutation={TOGGLE_CART_MUTATION}>
+    {toggleCart => (
+      <Query query={LOCAL_STATE_QUERY}>
+        {({ data }) => (
+          <CartStyle open={data.cartOpen}>
+            <header>
+              <CloseButton title="close" onClick={toggleCart}>
+                &times;
+              </CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p>You have ___ items in your cart.</p>
+            </header>
+            <footer>
+              <p>$10.10</p>
+              <SickButton>Checkout</SickButton>
+            </footer>
+          </CartStyle>
+        )}
+      </Query>
+    )}
+  </Mutation>
+);
 
 export default Cart;
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
